32b568c9cb493c1c39a17d6fd8ed7ebc4c301c08
Kelly Stevens <kllystvns@gmail.com>
2015-10-27 14:12:24 -0400

    SVG ripples!

--- public/ripple.js
+++ public/ripple.js
@@ -13,6 +13,9 @@ var scrollState = function(){
 function Ripple(center, growthFactor, vertices, amplitude, bounds, color){
 	this.color = color || '#00f';
 	this.amplitude = amplitude;
+	this.path = document.createElementNS('http://www.w3.org/2000/svg','path');
+	var svg = document.querySelector('#svg-background');
+	svg.appendChild(this.path);
 
 	if (bounds) {
 		var tl = bounds[0];
@@ -52,67 +55,62 @@ function Ripple(center, growthFactor, vertices, amplitude, bounds, color){
 		//5
 			{ p: [tl[0] + w + a, tl[1] - a], v: [1, initY], m: initMag },
 		//6
-			// { p: [tr[0] + a, tr[1]], v: [1, 0], m: initMag },
-		//7
 			{ p: [tr[0] + a, tr[1] + h1], v: [-1, initY], m: initMag / 1.5},
-		//8
+		//7
 			{ p: [tr[0] + a, tr[1] + h1 + h2], v: [1, initY], m: initMag / 1.5},
-		//9
+		//8
 			{ p: [tr[0] + a, tr[1] + h + a], v: [-1, initY], m: initMag },
-		//10
-			// { p: [br[0], br[1] + a], v: [0, 1], m: initMag },
-		//11
+		//9
 			{ p: [br[0] - w4, br[1] + a], v: [-1 * initX, -1], m: initMag},
-		//12
+		//10
 			{ p: [br[0] - w4 - w1, br[1] + a], v: [-1 * initX, 1], m: initMag},
-		//13
+		//11
 			{ p: [br[0] - w4 - w1 - w3, br[1] + a], v: [-1 * initX, -1], m: initMag},
-		//14
+		//12
 			{ p: [br[0] - w4 - w1 - w3 - w2, br[1] + a], v: [-1 * initX, 1], m: initMag},
-		//15
+		//13
 			{ p: [br[0] - w - a, br[1] + a], v: [-1, -1 * initY], m: initMag },
-		//16 
-			// { p: [bl[0] - a, bl[1]], v: [-1, 0], m: initMag },
-		//17
+		//14
 			{ p: [bl[0] - a, bl[1] - h1], v: [1, -1 * initY], m: initMag / 1.5},
-		//18
+		//15
 			{ p: [bl[0] - a, bl[1] - h1 - h2], v: [-1, -1 * initY], m: initMag / 1.5}
-		//19
-			// { p: [bl[0] - a, bl[1] - h], v: [1, 0], m: initMag }
 		];
-		// initial ROTATE vextor & AMP magnitude
-		_.each(this.vertices, function(vertex){
-			// vertex.m += this.amp(this.amplitude);
-			// Vector methods from P5
-			var v = createVector(vertex.v[0], vertex.v[1]);
-			v.rotate(0.1 - (Math.random() * 0.2));
-			vertex.v = [v.x, v.y];		
-		}, this);
+
+		// // initial ROTATE vextor & AMP magnitude
+		// _.each(this.vertices, function(vertex){
+		// 	// vertex.m += this.amp(this.amplitude);
+
+		// 	// Vector methods from P5
+		// 	var vector = createVector(vertex.v[0], vertex.v[1]);
+		// 	vector.rotate(0.1 - (Math.random() * 0.2));
+		// 	vertex.v = [vector.x, vector.y];		
+		// }, this);
 	}
 	else {
 		this.vertices = this.expand(center, vertices, growthFactor);
 	}
 }
 Ripple.prototype.expand = function(center, prevVertices, growthFactor){
-		// fix this
-		var vertices = prevVertices.map(function(v){
+		// creates duplicate object literal
+		var vertices = prevVertices.map(function(vert){
 			var copy = {p: [], v: [], m: null};
-			copy.p[0] = v.p[0];
-			copy.p[1] = v.p[1];
-			copy.v[0] = v.v[0];
-			copy.v[1] = v.v[1];
-			copy.m = v.m;
+			copy.p[0] = vert.p[0];
+			copy.p[1] = vert.p[1];
+			copy.v[0] = vert.v[0];
+			copy.v[1] = vert.v[1];
+			copy.m = vert.m;
 			return copy;
 		});
 		var c = center;
 		for (var i = 0; i < vertices.length; i++){
-			var v1 = createVector(vertices[i].p[0], vertices[i].p[1]);
-			var vc = createVector(c[0], c[1]);
-			var v2 = v1.sub(vc);
-			v2.setMag(growthFactor);
+			var dVector = [vertices[i].p[0] - c[0], (vertices[i].p[1] - c[1])];
+			var angle = Math.atan2(dVector[1], dVector[0]);
+			gVector = [Math.cos(angle), Math.sin(angle)];
+			gVector[0] *= growthFactor;
+			gVector[1] *= (growthFactor);
 
-			vertices[i].p[0] += v2.x;
-			vertices[i].p[1] += v2.y;
+			vertices[i].p[0] += gVector[0];
+			vertices[i].p[1] += gVector[1];
 		}
 		return vertices;
 }
@@ -120,32 +118,75 @@ Ripple.prototype.amp = function(amplitude){
 	return Math.random() * amplitude / 2 + amplitude;
 }
 Ripple.prototype.draw = function(){
-	var vertices = this.vertices;
-	// P5
-		noFill();
-		stroke(this.color);
-		strokeWeight(strokeGlobal);
-	for (var i = 0; i < vertices.length; i++) {
-		var j = i + 1;
-		if (i === vertices.length - 1) {
-			j = 0;
-		}
-		var px1 = vertices[i].p[0];
-		var py1 = vertices[i].p[1];
-		var px2 = vertices[j].p[0];
-		var py2 = vertices[j].p[1];
+	var verts = this.vertices;
+	// // P5
+	// 	noFill();
+	// 	stroke(this.color);
+	// 	strokeWeight(strokeGlobal);
+	// for (var i = 0; i < vertices.length; i++) {
+	// 	var j = i + 1;
+	// 	if (i === vertices.length - 1) {
+	// 		j = 0;
+	// 	}
+	// 	var px1 = vertices[i].p[0];
+	// 	var py1 = vertices[i].p[1];
+	// 	var px2 = vertices[j].p[0];
+	// 	var py2 = vertices[j].p[1];
 
-		var vec1 = createVector(vertices[i].v[0], vertices[i].v[1]);
-		vec1.setMag(vertices[i].m);
+	// 	var vec1 = createVector(vertices[i].v[0], vertices[i].v[1]);
+	// 	vec1.setMag(vertices[i].m);
 
-		var vec2 = createVector(vertices[j].v[0], vertices[j].v[1]);
-		vec2.setMag(vertices[j].m);
-		vec2.rotate(PI);
+	// 	var vec2 = createVector(vertices[j].v[0], vertices[j].v[1]);
+	// 	vec2.setMag(vertices[j].m);
+	// 	vec2.rotate(PI);
 
-		bezier(px1, py1, vec1.x + px1, vec1.y + py1, vec2.x + px2, vec2.y + py2, px2, py2);
+	// 	bezier(px1, py1, vec1.x + px1, vec1.y + py1, vec2.x + px2, vec2.y + py2, px2, py2);
+	// }
+
+	// (vector, point, magnitude, difference)
+	var addVector = function(vx,vy,px,py,m) {
+		var dx = vx * m;
+		var x = px + dx;
+		var dy = vy * m;
+		var y = py + dy;
+		// returns string
+		return x + ' ' + y;
 	}
-}
 
+	// Generate SVG Bezier curve syntax
+
+	// Moveto initial point
+	var M_xy = verts[0].p.join(' ');
+
+	// Curveto from 1st point to 2nd point
+	var C_cxcy_cxcy_xy;
+	var _cxcy1 = addVector(verts[0].v[0], verts[0].v[1], verts[0].p[0], verts[0].p[1], verts[0].m);
+	var _cxcy2 = addVector(-1 * verts[1].v[0], -1 * verts[1].v[1], verts[1].p[0], verts[1].p[1], verts[1].m); 
+	var _xy = verts[1].p.join(' ');
+	var C_cxcy_cxcy_xy = _cxcy1 + ' ' + _cxcy2 + ' ' + _xy; 
+
+	// Curveto (shorthand) to subsequent points
+	var S_cxcy_xy_array = [];
+	for (var i = 2; i < verts.length + 1; i++) {
+		var j = (i === verts.length ? 0 : i);
+		var _xy = verts[j].p.join(' ');
+		var _cxcy = addVector(-1 * verts[j].v[0], -1 * verts[j].v[1], verts[j].p[0], verts[j].p[1], verts[j].m);
+		
+		S_cxcy_xy_array.push('S ' + _cxcy + ' ' + _xy);
+	}
+	var S_cxcy_xy = S_cxcy_xy_array.join(' ');
+
+	var data = 'M ' + M_xy + ' C ' + C_cxcy_cxcy_xy + ' ' + S_cxcy_xy;
+
+	this.path.setAttribute('stroke', this.color);
+	this.path.setAttribute('stroke-width', strokeGlobal);
+	this.path.setAttribute('fill', 'none');
+	this.path.setAttribute('d', data);
+	return this.path;
+}
+Ripple.prototype.evaporate = function(){
+	this.path.remove();
+}
 Ripple.prototype.oscillate = function(){
 
 }
@@ -163,12 +204,12 @@ Ripple.prototype.oscillate = function(){
 function RippleGroup(domElement, scrollFactor, scrollEnd, growthFactor, amplitude, color){
 	this.el = document.querySelector(domElement);
 	this.color = color;
-	this.scrollStart = this.el.offsetTop - originY - 380;
-	this.scrollEnd = scrollEnd || this.scrollStart + this.el.offsetHeight + (380 * 2);
+	this.scrollStart = this.el.offsetTop - originY - 720;
+	this.scrollEnd = scrollEnd || this.scrollStart + this.el.offsetHeight + (720 * 2);
 	
 	this.scrollFactor = scrollFactor || 50;
 	this.highWaterMark = scrollState(); //default initial
-	this.prevGrowthFactor = growthFactor || 10;
+	this.prevGrowthFactor = growthFactor || 14;
 	this.growthFactor = function(){
 		return this.prevGrowthFactor * (1 + (this.ripples.length / 22))
 	}
@@ -202,7 +243,7 @@ RippleGroup.prototype.isActive = function(){
 
 //needs to happen on scroll
 RippleGroup.prototype.update = function(){
-	if (this.isActive) {
+	if (this.isActive()) {
 		// NOT READY YET
 		if (this.center() !== this.prevCenter) {
 			this.nudgeRipples();
@@ -240,6 +281,7 @@ RippleGroup.prototype.makeRipple = function(){
 	this.ripples.push(ripple);
 }
 RippleGroup.prototype.removeRipple = function(){
+	this.ripples[this.ripples.length -1].evaporate();
 	this.ripples.pop();
 }
 //needs to happen on frame
@@ -254,7 +296,7 @@ RippleGroup.prototype.oscillate = function(){
 
 //~~~ ON SCROLL ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-$(window).on('mousewheel', function(event){
+$(window).on('scroll', function(event){
 	bodyOfWater.forEach(function(rippleGroup){
 		rippleGroup.update();
 	});
@@ -263,27 +305,36 @@ $(window).on('mousewheel', function(event){
 
 
 
-function setup() {  // setup() runs once
-	createCanvas(window.innerWidth, 4600);
-  frameRate(30);
-}
+// function setup() {  // setup() runs once
+// 	createCanvas(window.innerWidth, 4600);
+//   frameRate(30);
+// }
+
 
 //~~~ ON FRAME ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-function draw() {  // draw() loops forever, until stopped
-	clear();
-
+window.draw = function(){
 	if (window.bodyOfWater) {
 		bodyOfWater.forEach(function(rippleGroup){
 			rippleGroup.ripples.forEach(function(ripple){
 				ripple.draw();
-				// noLoop();
-			})	
-		});
+			})
+		})
 	}
-	//blendMode()
-
 }
+drawID = setInterval(draw, 100);
+
+// function draw() {  // draw() loops forever, until stopped
+// 	clear();
+// 	if (window.bodyOfWater) {
+// 		bodyOfWater.forEach(function(rippleGroup){
+// 			rippleGroup.ripples.forEach(function(ripple){
+// 				ripple.draw();
+// 				// noLoop();
+// 			})	
+// 		});
+// 	}
+// 	//blendMode()
+// }
 
 
 
